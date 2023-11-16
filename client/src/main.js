56bf4dc7d8c32ead948d655b566c048a6513b4b7
oooihmm <oooihmm@naver.com>
2023-11-16 14:19:54 +0900

    Feat: main.js 설정

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,5 @@
-import Vue from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import router from "./router/index";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).use(router).mount("#app");
