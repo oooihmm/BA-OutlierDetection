27fb266aaf3c54705ad96f7558eaff89fba5547d
oooihmm <oooihmm@naver.com>
2023-11-16 14:20:46 +0900

    Feat: Navigation 생성

---
+++ client/src/components/NavigationBar.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="navigation">
+    <span>
+      <img
+        src="../assets/OutlierDetection.png"
+        alt="Outlier Detection"
+        @click="
+          () => {
+            this.$router.push('/');
+          }
+        "
+      />
+    </span>
+    <div>
+      <span>
+        <img
+          class="image"
+          src="../assets/Home.png"
+          alt="Home"
+          @click="
+            () => {
+              this.$router.push('/');
+            }
+          "
+        />
+      </span>
+      <span>
+        <img
+          class="image"
+          src="../assets/Statistics.png"
+          alt="Statistics"
+          @click="
+            () => {
+              this.$router.push('/statistics');
+            }
+          "
+        />
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+};
+</script>
+
+<style scoped>
+#navigation {
+  max-width: 100vw;
+  height: 70px;
+  padding: 0 30px;
+  background-color: #000583;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.image {
+  padding: 0 5px;
+}
+</style>
