6f0b2d3fc86b9b05f19433c1c8c6ea0b7863a657
oooihmm <oooihmm@naver.com>
2023-12-07 21:12:15 +0900

    Feat: 로딩 페이지 수정

--- client/src/views/LoadingPage.vue
+++ client/src/views/LoadingPage.vue
@@ -1,62 +1,74 @@
 <template>
-    <div>
-        <Navigation />
-        <div class="loading_image">
-            <img src="../assets/Loading.png" width="150" class="icon">
-            <div class="loading_image_text">분석 중입니다 ...</div>
-            <div class="eng_text">Outlier Detection</div>
-        </div>
+  <div>
+    <Navigation />
+    <div class="loading_image">
+      <img src="../assets/Loading.png" width="150" class="icon" />
+      <div class="loading_image_text">분석 중입니다 ...</div>
+      <div class="eng_text">Outlier Detection</div>
     </div>
+  </div>
 </template>
 
 <script>
 import Navigation from "../components/NavigationBar.vue";
 
 export default {
-    name: "LoadingPage",
-    components: {
-        Navigation,
-    },
+  name: "LoadingPage",
+  components: {
+    Navigation,
+  },
+  mounted() {
+    const file = history.state.file;
+    const back = history.state.back;
+    const forward = history.state.forward;
+    if (file && back === "/" && forward === null) {
+      setTimeout(() => {
+        this.$router.push("/statistics");
+      }, 4000);
+    } else {
+      alert("유효하지 않은 요청입니다.");
+      this.$router.push("/");
+    }
+  },
 };
 </script>
 
-
 <style>
-.loading_image{
-    position: relative;
-    text-align: center;
-    font-family: Arial, sans-serif;
-    padding-top: 550px;
+.loading_image {
+  position: relative;
+  text-align: center;
+  font-family: Arial, sans-serif;
+  padding-top: 550px;
 }
 
-.icon{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 150px;
-    height: auto;
-    z-index: 1;
-    opacity: 0.3;
+.icon {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 150px;
+  height: auto;
+  z-index: 1;
+  opacity: 0.3;
 }
 
-.loading_image_text{
-    font-size: 45px; 
-    color: black; 
-    position: absolute;
-    width: 100%;
-    top: 50%;
-    transform: translateY(-50%);
-    margin: 0;
-    z-index: 2;
+.loading_image_text {
+  font-size: 45px;
+  color: black;
+  position: absolute;
+  width: 100%;
+  top: 50%;
+  transform: translateY(-50%);
+  margin: 0;
+  z-index: 2;
 }
 
-.eng_text{
-    font-size: 30px;
-    color: #d5d5d5;
-    position: absolute;
-    top: calc(50% + 50px); 
-    width: 100%;
-    transform: translateY(200%);
+.eng_text {
+  font-size: 30px;
+  color: #d5d5d5;
+  position: absolute;
+  top: calc(50% + 50px);
+  width: 100%;
+  transform: translateY(200%);
 }
 </style>
